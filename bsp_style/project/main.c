90abff35ff948d54b07e385acb3f65383f8208ee
PromisingVI <458386139@qq.com>
2024-05-16 18:09:11 +0800

    add touch screen
    【变更描述】
    触摸屏驱动
    
    Signed-off-by: PromisingVI <458386139@qq.com>

--- bsp_style/project/main.c
+++ bsp_style/project/main.c
@@ -23,6 +23,8 @@ Copyright © zuozhongkai Co., Ltd. 1998-2019. All rights reserved.
 #include "bsp_i2c.h"
 #include "bsp_ap3216c.h"
 #include "bsp_icm20608.h"
+#include "bsp_ft5xx6.h"
+#include "bsp_gt9147.h"
 
 /* 背景颜色索引 */
 unsigned int backcolor[10] = {
@@ -108,6 +110,7 @@ void decimals_display(unsigned short x, unsigned short y, unsigned char size, si
  */
 int main(void)
 {
+ 	unsigned char i = 0;
 	unsigned char state = OFF;
 
 	imx6ul_hardfpu_enable();	/* 使能I.MX6U的硬件浮点 			*/
@@ -119,72 +122,69 @@ int main(void)
 	beep_init();				/* 初始化beep	 				*/
 	uart_init();				/* 初始化串口，波特率115200 */
 	lcd_init();					/* 初始化LCD 					*/		
+	
+	/* 初始化触摸屏					*/ 
+	gt9147_init();
+	if(gt_init_fail==1) //判断gt系列初始化是否失败
+	{
+		ft5426_init();
+	}
 
 	tftlcd_dev.forecolor = LCD_RED;
-	lcd_show_string(50, 10, 400, 24, 24, (char*)"ALPHA-IMX6U SPI TEST");  
-	lcd_show_string(50, 40, 200, 16, 16, (char*)"ICM20608 TEST");  
+	lcd_show_string(50, 10, 400, 24, 24, (char*)"IMX6U-ALPHA TOUCH SCREEN TEST");  
+	lcd_show_string(50, 40, 200, 16, 16, (char*)"TOUCH SCREEN TEST");  
 	lcd_show_string(50, 60, 200, 16, 16, (char*)"ATOM@ALIENTEK");  
 	lcd_show_string(50, 80, 200, 16, 16, (char*)"2019/3/27");  
 	
-	while(icm20608_init())		/* 初始化ICM20608	 			*/
-	{
-		lcd_show_string(50, 100, 200, 16, 16, (char*)"ICM20608 Check Failed!");
-		delayms(500);
-		lcd_show_string(50, 100, 200, 16, 16, (char*)"Please Check!        ");
-		delayms(500);
-	}	
-
-	lcd_show_string(50, 100, 200, 16, 16, (char*)"ICM20608 Ready");
-	
-	lcd_show_string(50, 130, 200, 16, 16, (char*)"accel x:");  
-	lcd_show_string(50, 150, 200, 16, 16, (char*)"accel y:");  
-	lcd_show_string(50, 170, 200, 16, 16, (char*)"accel z:");  
-	lcd_show_string(50, 190, 200, 16, 16, (char*)"gyro  x:"); 
-	lcd_show_string(50, 210, 200, 16, 16, (char*)"gyro  y:"); 
-	lcd_show_string(50, 230, 200, 16, 16, (char*)"gyro  z:"); 
-	lcd_show_string(50, 250, 200, 16, 16, (char*)"temp   :"); 
-
-	lcd_show_string(50 + 181, 130, 200, 16, 16, (char*)"g");  
-	lcd_show_string(50 + 181, 150, 200, 16, 16, (char*)"g");  
-	lcd_show_string(50 + 181, 170, 200, 16, 16, (char*)"g");  
-	lcd_show_string(50 + 181, 190, 200, 16, 16, (char*)"o/s"); 
-	lcd_show_string(50 + 181, 210, 200, 16, 16, (char*)"o/s"); 
-	lcd_show_string(50 + 181, 230, 200, 16, 16, (char*)"o/s"); 
-	lcd_show_string(50 + 181, 250, 200, 16, 16, (char*)"C");
-	
+	lcd_show_string(50, 110, 400, 16, 16,	(char*)"TP Num	:");  
+	lcd_show_string(50, 130, 200, 16, 16,	(char*)"Point0 X:");  
+	lcd_show_string(50, 150, 200, 16, 16,	(char*)"Point0 Y:");  
+	lcd_show_string(50, 170, 200, 16, 16,	(char*)"Point1 X:");  
+	lcd_show_string(50, 190, 200, 16, 16,	(char*)"Point1 Y:");  
+	lcd_show_string(50, 210, 200, 16, 16,	(char*)"Point2 X:");  
+	lcd_show_string(50, 230, 200, 16, 16,	(char*)"Point2 Y:");  
+	lcd_show_string(50, 250, 200, 16, 16,	(char*)"Point3 X:");  
+	lcd_show_string(50, 270, 200, 16, 16,	(char*)"Point3 Y:");  
+	lcd_show_string(50, 290, 200, 16, 16,	(char*)"Point4 X:");  
+	lcd_show_string(50, 310, 200, 16, 16,	(char*)"Point4 Y:");  
 	tftlcd_dev.forecolor = LCD_BLUE;
-
+	
 	while(1)					
-	{		
-		icm20608_getdata();
-		integer_display(50 + 70, 130, 16, icm20608_dev.accel_x_adc);
-		integer_display(50 + 70, 150, 16, icm20608_dev.accel_y_adc);
-		integer_display(50 + 70, 170, 16, icm20608_dev.accel_z_adc);
-		integer_display(50 + 70, 190, 16, icm20608_dev.gyro_x_adc);
-		integer_display(50 + 70, 210, 16, icm20608_dev.gyro_y_adc);
-		integer_display(50 + 70, 230, 16, icm20608_dev.gyro_z_adc);
-		integer_display(50 + 70, 250, 16, icm20608_dev.temp_adc);
-
-		decimals_display(50 + 70 + 50, 130, 16, icm20608_dev.accel_x_act);
-		decimals_display(50 + 70 + 50, 150, 16, icm20608_dev.accel_y_act);
-		decimals_display(50 + 70 + 50, 170, 16, icm20608_dev.accel_z_act);
-		decimals_display(50 + 70 + 50, 190, 16, icm20608_dev.gyro_x_act);
-		decimals_display(50 + 70 + 50, 210, 16, icm20608_dev.gyro_y_act);
-		decimals_display(50 + 70 + 50, 230, 16, icm20608_dev.gyro_z_act);
-		decimals_display(50 + 70 + 50, 250, 16, icm20608_dev.temp_act);
-
-#if 0		
-		printf("accel x = %d\r\n",icm20608_dev.accel_x_adc);
-		printf("accel y = %d\r\n",icm20608_dev.accel_y_adc);
-		printf("accel z = %d\r\n",icm20608_dev.accel_z_adc);
-		printf("gyrp  x = %d\r\n",icm20608_dev.gyro_x_adc);
-		printf("gyro  y = %d\r\n",icm20608_dev.gyro_y_adc);
-		printf("gyro  z = %d\r\n",icm20608_dev.gyro_z_adc);
-		printf("temp    = %d\r\n",icm20608_dev.temp_adc);
-#endif
-		delayms(120);
-		state = !state;
-		led_switch(LED0,state);	
+	{
+		if(gt_init_fail==1) {
+			lcd_shownum(50 + 72, 110, ft5426_dev.point_num , 1, 16);
+			lcd_shownum(50 + 72, 130, ft5426_dev.x[0], 5, 16);
+			lcd_shownum(50 + 72, 150, ft5426_dev.y[0], 5, 16);
+			lcd_shownum(50 + 72, 170, ft5426_dev.x[1], 5, 16);
+			lcd_shownum(50 + 72, 190, ft5426_dev.y[1], 5, 16);
+			lcd_shownum(50 + 72, 210, ft5426_dev.x[2], 5, 16);
+			lcd_shownum(50 + 72, 230, ft5426_dev.y[2], 5, 16);
+			lcd_shownum(50 + 72, 250, ft5426_dev.x[3], 5, 16);
+			lcd_shownum(50 + 72, 270, ft5426_dev.y[3], 5, 16);
+			lcd_shownum(50 + 72, 290, ft5426_dev.x[4], 5, 16);
+			lcd_shownum(50 + 72, 310, ft5426_dev.y[4], 5, 16);
+		} else {
+			lcd_shownum(50 + 72, 110, gt9147_dev.point_num , 1, 16);
+			lcd_shownum(50 + 72, 130, gt9147_dev.x[0], 5, 16);
+			lcd_shownum(50 + 72, 150, gt9147_dev.y[0], 5, 16);
+			lcd_shownum(50 + 72, 170, gt9147_dev.x[1], 5, 16);
+			lcd_shownum(50 + 72, 190, gt9147_dev.y[1], 5, 16);
+			lcd_shownum(50 + 72, 210, gt9147_dev.x[2], 5, 16);
+			lcd_shownum(50 + 72, 230, gt9147_dev.y[2], 5, 16);
+			lcd_shownum(50 + 72, 250, gt9147_dev.x[3], 5, 16);
+			lcd_shownum(50 + 72, 270, gt9147_dev.y[3], 5, 16);
+			lcd_shownum(50 + 72, 290, gt9147_dev.x[4], 5, 16);
+			lcd_shownum(50 + 72, 310, gt9147_dev.y[4], 5, 16);
+		}
+		delayms(10);
+		i++;
+	
+		if(i == 50)
+		{	
+			i = 0;
+			state = !state;
+			led_switch(LED0,state); 
+		}
 	}
 	return 0;
 }
